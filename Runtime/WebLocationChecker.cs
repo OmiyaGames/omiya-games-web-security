5c700fe26d1234e27b1d45849547d3ded76d540e
Taro Omiya <taro.omiya@omiyagames.com>
2020-05-21 00:32:55 -0400

    Adding documentation to public methods and properties in WebLocationChecker

--- Runtime/WebLocationChecker.cs
+++ Runtime/WebLocationChecker.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using OmiyaGames.Cryptography;
 using OmiyaGames.Global;
 
@@ -58,14 +57,18 @@ namespace OmiyaGames.Web.Security
     /// <strong>Date:</strong> 2/12/2019<br/>
     /// <strong>Author:</strong> Taro Omiya
     /// </term>
-    /// <description>Adding encryption support of the binary <see cref="DomainList"/>.</description>
+    /// <description>
+    /// Adding encryption support of the binary <see cref="DomainList"/>.
+    /// </description>
     /// </item><item>
     /// <term>
     /// <strong>Version:</strong> 0.1.0-preview.1<br/>
     /// <strong>Date:</strong> 5/20/2020<br/>
     /// <strong>Author:</strong> Taro Omiya
     /// </term>
-    /// <description>Converting to package.</description>
+    /// <description>
+    /// Converting to package. Improving documentation for DocFX support.
+    /// </description>
     /// </item>
     /// </list>
     /// </remarks>
@@ -77,44 +80,73 @@ namespace OmiyaGames.Web.Security
     /// </para><para>
     /// Extra modifications by jcx from Github:<br/>
     /// https://gist.github.com/jcx/93a3fc93531911add8a8
-    /// </para>
+    /// </para><para>
     /// Add this script to an object in the first scene of your game.
-    /// It doesn't do anything for non-webplayer builds. For webplayer
-    /// builds, it checks the domain to make sure it contains at least
-    /// one of the strings, or it will redirect the page to the proper
-    /// URL for the game.
+    /// For WebGL builds, this script grabs the domain the game is running on,
+    /// and verifies it against two lists:
+    /// </para><para>
+    /// 1) The list of strings in <see cref="DefaultDomainList"/>, or
+    /// </para><para>
+    /// 2) If a <see cref="DomainList"/> was successfully downloaded from
+    /// <see cref="RemoteDomainListUrl"/>, matches the domain to any of the
+    /// globs listed in the <see cref="DomainList"/>. Remember that if the
+    /// <see cref="DomainList"/> is encrypted, to set the
+    /// <see cref="domainDecrypter"/> in the Unity inspector.
+    /// </para><para>
+    /// Used as follows:
+    /// </para>
+    /// <code>
+    /// IEnumerator Start()
+    /// {
+    ///     WebLocationChecker checker = GetComponent<WebLocationChecker>();
+    ///     yield return StartCoroutine(checker.CheckDomainList());
+    ///     Debug.Log(checker.CurrentState);
+    /// }
+    /// </code>
+    /// <para>
+    /// If the script is attached to a <see cref="GameObject"/> with
+    /// <see cref="Singleton"/> already attached, the above example code
+    /// will run automatically on <see cref="SceneAwake"/>.
+    /// </para>
     /// </summary>
     [DisallowMultipleComponent]
     public class WebLocationChecker : ISingletonScript
     {
         /// <summary>
-        /// 
+        /// Header string for the Unity Inspector.
         /// </summary>
         public const string RemoteDomainListHeader = "Remote Domain List";
 
         /// <summary>
-        /// 
+        /// Indicates progression and result of this script.
         /// </summary>
         public enum State : short
         {
             /// <summary>
-            /// 
+            /// Indicates the script hasn't verified the domain yet.
             /// </summary>
             NotUsed = -1,
             /// <summary>
-            /// 
+            /// Indicates the script is in the middle of evaluating
+            /// the domain the WebGL build is running on. This process
+            /// can include downloading a <see cref="DomainList"/>.
             /// </summary>
             InProgress = 0,
             /// <summary>
-            /// 
+            /// Indicates there were trouble verifying the domain.
+            /// Most likely, this is due to being unable to download
+            /// a <see cref="DomainList"/> for verification.
             /// </summary>
             EncounteredError,
             /// <summary>
-            /// 
+            /// Indicates the domain this WebGL build is running on
+            /// is valid, matching an expected glob/string.
             /// </summary>
             DomainMatched,
             /// <summary>
-            /// 
+            /// Indicates the domain this WebGL build is running on
+            /// is <em>not</em> valid:
+            /// it doesn't match any expected glob/string.
             /// </summary>
             DomainDidntMatch
         }
@@ -122,9 +154,11 @@ namespace OmiyaGames.Web.Security
 #if UNITY_WEBGL
         /// <summary>
         /// Redirects a WebGL build to a specific page.
-        /// This will <em>not</em> open a new tab or window.
+        /// This will <em>not</em> open a new tab or window.<br/>
+        /// Note: depending on the <iframe> setting, this may cause
+        /// an Access Denied error.
         /// </summary>
-        /// <param name="url"></param>
+        /// <param name="url">URL to redirect to.</param>
         [DllImport("__Internal")]
         public static extern void RedirectTo(string url);
 #endif
@@ -145,7 +179,8 @@ namespace OmiyaGames.Web.Security
         [Tooltip("[optional] The URL to fetch a list of domains")]
         private string remoteDomainListUrl;
         /// <summary>
-        /// 
+        /// [optional] <see cref="StringCryptographer"/> to decrypt
+        /// the downloaded <see cref="DomainList"/>.
         /// </summary>
         [SerializeField]
         [Tooltip("[optional] The cryptographer that decrypts the encrypted strings in the list of domains")]
@@ -175,7 +210,8 @@ namespace OmiyaGames.Web.Security
 
         #region Properties
         /// <summary>
-        /// 
+        /// Indicates the state this script is in.
+        /// <seealso cref="State"/>
         /// </summary>
         public State CurrentState
         {
@@ -184,7 +220,8 @@ namespace OmiyaGames.Web.Security
         } = State.NotUsed;
 
         /// <summary>
-        /// 
+        /// Indicates if a <see cref="DomainList"/> was
+        /// downloaded or not.
         /// </summary>
         public bool IsDomainListSuccessfullyDownloaded
         {
@@ -195,7 +232,7 @@ namespace OmiyaGames.Web.Security
         }
 
         /// <summary>
-        /// 
+        /// The domain this WebGL build is running on.
         /// </summary>
         public string RetrievedHostName
         {
@@ -206,7 +243,9 @@ namespace OmiyaGames.Web.Security
         }
 
         /// <summary>
-        /// 
+        /// The default list of domains to match <see cref="RetrievedHostName"/> against.
+        /// This list excludes the globs stored in a <see cref="DomainList"/>.
+        /// <seealso cref="RemoteDomainListUrl"/>
         /// </summary>
         public string[] DefaultDomainList
         {
@@ -217,7 +256,7 @@ namespace OmiyaGames.Web.Security
         }
 
         /// <summary>
-        /// 
+        /// The list of globs in a <see cref="DomainList"/>, if successfully downloaded.
         /// </summary>
         public string[] DownloadedDomainList
         {
@@ -226,7 +265,8 @@ namespace OmiyaGames.Web.Security
         } = null;
 
         /// <summary>
-        /// 
+        /// The URL used to download a <see cref="DomainList"/>. Slightly randomized
+        /// to prevent predictablity.
         /// </summary>
         public string DownloadDomainsUrl
         {
@@ -235,7 +275,9 @@ namespace OmiyaGames.Web.Security
         } = null;
 
         /// <summary>
-        /// 
+        /// The <see cref="Dictionary{TKey, TValue}"/> of all unique domains.
+        /// Keys are the globs, and values are the regular expression equivalent.
+        /// <seealso cref="DomainList.ConvertToRegex(string, StringBuilder)"/>
         /// </summary>
         public Dictionary<string, Regex> AllUniqueDomains
         {
@@ -243,7 +285,7 @@ namespace OmiyaGames.Web.Security
         } = new Dictionary<string, Regex>();
 
         /// <summary>
-        /// 
+        /// Indicates the error when attempting to download a <see cref="DomainList"/>.
         /// </summary>
         public string DownloadErrorMessage
         {
@@ -252,14 +294,13 @@ namespace OmiyaGames.Web.Security
         } = null;
 
         /// <summary>
-        /// 
+        /// The original URL to download a <see cref="DomainList"/> from.
+        /// <seealso cref="DownloadDomainsUrl"/>
         /// </summary>
         public string RemoteDomainListUrl => remoteDomainListUrl;
         #endregion
 
-        /// <summary>
-        /// 
-        /// </summary>
+        /// <inheritdoc/>
         public override void SingletonAwake()
         {
             if (Singleton.Instance.IsWebApp == true)
@@ -276,7 +317,8 @@ namespace OmiyaGames.Web.Security
         }
 
         /// <summary>
-        /// 
+        /// Makes the build redirect the browser to <see cref="redirectURL"/>.
+        /// <seealso cref="RedirectTo(String)"/>
         /// </summary>
         public void ForceRedirect()
         {
@@ -286,6 +328,54 @@ namespace OmiyaGames.Web.Security
                 RedirectTo(redirectURL);
             }
 #endif
+        }
+
+        /// <summary>
+        /// Coroutine to start the whole verification process.
+        /// Don't forget to call it with
+        /// <see cref="MonoBehaviour.StartCoroutine(IEnumerator)"/>, like the example below:
+        /// <code>
+        /// WebLocationChecker checker = GetComponent<WebLocationChecker>();
+        /// StartCoroutine(checker.CheckDomainList());
+        /// </code>
+        /// </summary>
+        /// <returns>An enumerator for a coroutine.</returns>
+        public IEnumerator CheckDomainList()
+        {
+            // Setup variables
+            StringBuilder buf = new StringBuilder();
+            DownloadedDomainList = null;
+            DownloadDomainsUrl = null;
+            DownloadErrorMessage = null;
+
+            // Update state
+            CurrentState = State.InProgress;
+
+            // Deactivate any objects
+            SetWaitObjectActive(false);
+
+            // Grab a domain list remotely
+            if (string.IsNullOrEmpty(RemoteDomainListUrl) == false)
+            {
+                // Grab remote domain list
+                DownloadDomainsUrl = GenerateRemoteDomainList(buf);
+                yield return StartCoroutine(DownloadRemoteDomainList(buf, DownloadDomainsUrl));
+            }
+
+            // Setup hashset
+            PopulateAllUniqueDomains(buf, AllUniqueDomains, DefaultDomainList, DownloadedDomainList);
+
+            // Make sure there's at least one domain we need to check
+            CurrentState = GetNewState(AllUniqueDomains, out retrievedHostName);
+
+            // Reactivate any objects
+            SetWaitObjectActive(true);
+
+            // Check if we should force redirecting the player
+            if ((forceRedirectIfDomainDoesntMatch == true) && (IsDomainInvalid(CurrentState) == true))
+            {
+                ForceRedirect();
+            }
         }
 
         #region Helper Static Methods
@@ -432,44 +522,6 @@ namespace OmiyaGames.Web.Security
                         DownloadErrorMessage = ex.Message;
                     }
                 }
-            }
-        }
-
-        IEnumerator CheckDomainList()
-        {
-            // Setup variables
-            StringBuilder buf = new StringBuilder();
-            DownloadedDomainList = null;
-            DownloadDomainsUrl = null;
-            DownloadErrorMessage = null;
-
-            // Update state
-            CurrentState = State.InProgress;
-
-            // Deactivate any objects
-            SetWaitObjectActive(false);
-
-            // Grab a domain list remotely
-            if (string.IsNullOrEmpty(RemoteDomainListUrl) == false)
-            {
-                // Grab remote domain list
-                DownloadDomainsUrl = GenerateRemoteDomainList(buf);
-                yield return StartCoroutine(DownloadRemoteDomainList(buf, DownloadDomainsUrl));
-            }
-
-            // Setup hashset
-            PopulateAllUniqueDomains(buf, AllUniqueDomains, DefaultDomainList, DownloadedDomainList);
-
-            // Make sure there's at least one domain we need to check
-            CurrentState = GetNewState(AllUniqueDomains, out retrievedHostName);
-
-            // Reactivate any objects
-            SetWaitObjectActive(true);
-
-            // Check if we should force redirecting the player
-            if ((forceRedirectIfDomainDoesntMatch == true) && (IsDomainInvalid(CurrentState) == true))
-            {
-                ForceRedirect();
             }
         }
 
